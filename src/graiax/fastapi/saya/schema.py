e3d333c1fd51d6034e7c670ea6f67c84b03a850b
Redlnn <w731347477@gmail.com>
2023-08-06 14:14:27 +0800

    :sparkles: 0.3.0

--- src/graiax/fastapi/saya/schema.py
+++ src/graiax/fastapi/saya/schema.py
@@ -1,25 +1,15 @@
+from __future__ import annotations
+
 from dataclasses import dataclass, field
 from enum import Enum
-from typing import (
-    Any,
-    Callable,
-    Dict,
-    List,
-    Literal,
-    Optional,
-    Sequence,
-    Set,
-    Type,
-    Union,
-)
+from typing import Any, Callable, Literal, Sequence, TypeVar
 
-from fastapi.encoders import DictIntStrAny, SetIntStr
+from fastapi import routing
 from fastapi.params import Depends
-from fastapi.routing import APIRoute
+from fastapi.types import IncEx
 from fastapi.utils import generate_unique_id
 from graia.saya.schema import BaseSchema
 from starlette.responses import JSONResponse, Response
-from starlette.routing import BaseRoute
 
 Method = Literal["GET", "PUT", "POST", "DELETE", "OPTIONS", "HEAD", "PATCH", "TRACE"]
 
@@ -27,35 +17,32 @@ Method = Literal["GET", "PUT", "POST", "DELETE", "OPTIONS", "HEAD", "PATCH", "TR
 @dataclass
 class RouteSchema(BaseSchema):
     path: str
-    methods: Union[List[Method], Set[Method]]
+    methods: list[Method]
     response_model: Any = None
-    status_code: Optional[int] = None
-    tags: Optional[List[Union[str, Enum]]] = None
-    dependencies: Optional[Sequence[Depends]] = None
-    summary: Optional[str] = None
-    description: Optional[str] = None
+    status_code: int | None = None
+    tags: list[str | Enum] | None = None
+    dependencies: Sequence[Depends] | None = None
+    summary: str | None = None
+    description: str | None = None
     response_description: str = "Successful Response"
-    responses: Optional[Dict[Union[int, str], Dict[str, Any]]] = None
-    deprecated: Optional[bool] = None
-    operation_id: Optional[str] = None
-    response_model_include: Optional[Union[SetIntStr, DictIntStrAny]] = None
-    response_model_exclude: Optional[Union[SetIntStr, DictIntStrAny]] = None
+    responses: dict[int | str, dict[str, Any]] | None = None
+    deprecated: bool | None = None
+    operation_id: str | None = None
+    response_model_include: IncEx | None = None
+    response_model_exclude: IncEx | None = None
     response_model_by_alias: bool = True
     response_model_exclude_unset: bool = False
     response_model_exclude_defaults: bool = False
     response_model_exclude_none: bool = False
     include_in_schema: bool = True
-    response_class: Type[Response] = field(default=JSONResponse)
-    name: Optional[str] = None
-    route_class_override: Optional[Type[APIRoute]] = None
-    callbacks: Optional[List[BaseRoute]] = None
-    openapi_extra: Optional[Dict[str, Any]] = None
-    generate_unique_id_function: Callable[[APIRoute], str] = field(
-        default=generate_unique_id
-    )
+    response_class: type[Response] = field(default=JSONResponse)
+    name: str | None = None
+    openapi_extra: dict[str, Any] | None = None
+    generate_unique_id_function: Callable[[routing.APIRoute], str] = field(default=generate_unique_id)
 
 
 @dataclass
 class WebsocketRouteSchema(BaseSchema):
     path: str
-    name: Optional[str] = None
+    name: str | None = None
+    dependencies: Sequence[Depends] | None = None
